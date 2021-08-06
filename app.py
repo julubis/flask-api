9b410957949fd62a65584576b27d4af9f463f5a5
julubis <julubis@github.com>
2021-08-06 15:22:58 +0700

    test ping

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from time import time
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
+r = requests.Session()
 class Hello(Resource):
 	def get(self):
-		return {"hello":"world"}
+		ts = time()
+		for i in range(100):
+			r.get("https://shopee.co.id/")
+		res = time() - ts
+		return str(res)
 		
 api.add_resource(Hello,"/")
 if __name__ == "__main__":
